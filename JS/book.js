41ca8ff16519750e322c02ad4c211220d957e144
Sabbir Ahmed <sabbirahmed.probd@gmail.com>
2021-09-02 11:58:15 +0600

    third commit

--- JS/book.js
+++ JS/book.js
@@ -1,62 +1,92 @@
+/* global variable declaration */
+
 const searchButton = document.getElementById('search-btn');
 const searchInput = document.getElementById('searchInput');
 const booksContainer = document.getElementById("books-container");
 const errorContainer = document.getElementById('error')
+const showResult = document.getElementById('result-number')
+
 searchButton.addEventListener('click', function(){
-    console.log("found")
+    
     const searchText = searchInput.value;
+
+    /* error handling */
+
     if(searchText === ""){
         errorContainer.innerText = "Please write a book name.";
+
+        /* clearing previous result */
+
+        booksContainer.innerHTML = "";
+        showResult.innerHTML = "";
         return;
     };
-     
-    console.log(searchText);
-    //clear
-    booksContainer.innerHTML = "";
 
-    const url = `https://openlibrary.org/search.json?q=${searchText}`
+    /* clearing previous result */
+
+    booksContainer.innerHTML = "";
+    showResult.innerHTML = "";
+    errorContainer.innerHTML= "";
+
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => loadBooks(data.docs))
+    .then(data => showBooks(data.docs))
      
     .finally(() => {
+
+      /* clearing search input field */
+
         searchInput.value = "";
         
       });
-})
+      errorContainer.innerHTML = "";
+});
 
-// load books
-const loadBooks = (booksArray) =>{
-    console.log(booksArray)
-   const books = booksArray.filter(filtered => filtered.book.author_name[0] !== undefined && filtered.book.author_name[0] !== undefined && filtered.book.first_publish_year !== undefined && filtered.book.publisher[0]);
-   console.log(books)
+/* loop through books and showing on UI */
+
+const showBooks = (booksArray) =>{
+
+  /* filtering books which has valid information only and showing it on UI */
+
+   const books = booksArray.filter(filtered => filtered.cover_i !== undefined && filtered.title !== undefined && filtered.author_name !== undefined && filtered.first_publish_year !== undefined && filtered.publisher  !== undefined ) ;
+  /* error handling */
    if(books.length === 0){
     
     errorContainer.innerHTML = "No Result Found"
    }
    else{
 
+     /* showing how much books user got */
+
+   const resultNumber = document.createElement('h5')
+   resultNumber.innerHTML = `You are seeing ${books.length} books of ${booksArray.length}`
+   showResult.appendChild(resultNumber);
+
+   /* loop through filtered books using forEach method */
 
     books.forEach(book =>{
         
         const div = document.createElement("div");
-        div.classList.add("col-4")
+        div.classList.add("col-4");
+        
+        /* books card */
         div.innerHTML =`
-        <div class="card mb-3 bg-info" style="max-width: 440px;">
+        <div class="card mb-3 bg-secondary" style="max-width: 440px; max-height-500px">
         <div class="row g-0">
-          <div class="col-md-6">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid rounded-start" alt="...">
-          </div>
-          <div class="col-md-6">
+          
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid card-img-top" alt="...">
+          
+          
             <div class="card-body">
-              <h3 class="card-title">${book.title}</h3>
-              <h5 class="card-text">Author: ${book.author_name[0]} </h5>
-              <p class="card-text">First Publish: ${book.first_publish_year}</p>
-              <p class="card-text">Publisher: ${book.publisher[0]}</p>
+              <h3 class="card-title text-white">${book.title}</h3>
+              <h5 class="card-text text-warning">Author: ${book.author_name[0]} </h5>
+              <p class="card-text text-white">First Publish: ${book.first_publish_year}</p>
+              <p class="card-text text-warning">Publisher: ${book.publisher[0]}</p>
+            
             </div>
-          </div>
         </div>
-      </div>
+        </div>
         `;
         booksContainer.appendChild(div);
     });
